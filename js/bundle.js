22fc8538a535736da52e8bebd654666c84a142d7
Florian Krönert <Florian.Kroenert@gmx.de>
2017-07-23 12:32:51 +0200

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/Smogon-Team-Assistance/";
+/******/ 	__webpack_require__.p = "/Smogon-Team-Assistant/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 134);
